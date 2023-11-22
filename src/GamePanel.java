1a2f9c88973d02e7693b2e0ee0e6a03dc990de12
Rohma Wajid <gusrasuro@student.gu.se>
2023-11-22 17:11:23 +0100

    added KeyAdapter and a Toxic Food

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -16,19 +18,26 @@ public class GamePanel extends JPanel implements ActionListener {
     private int bodyUnits;
     private int foodX;
     private int foodY;
+    private int ToxicfoodX;
+    private int ToxicfoodY;
     private String direction;
     private boolean running;
     private Random random;
     private Timer timer;
+    private int foodCounter;
+    private int num; //better name
 
     public GamePanel() {
         this.bodyUnits = 6;
+        this.foodCounter = 0;
         this.direction = "Right";
         this.running = false;
         this.random = new Random();
+        this.num = random.nextInt(10);
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
 
         startGame();
 
@@ -37,7 +46,9 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-        draw(graphics);
+        if (running) { //Does this work??
+            draw(graphics);
+        }
     }
 
     public void draw(Graphics graphics) {
@@ -51,6 +62,13 @@ public class GamePanel extends JPanel implements ActionListener {
         //food item
         graphics.setColor(Color.orange);
         graphics.fillOval(foodX, foodY, UNIT, UNIT);
+        //Food one item
+        if (foodCounter == num) {
+            graphics.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+            graphics.fillOval(ToxicfoodX, ToxicfoodY, UNIT, UNIT);
+        }
+
+
 
         //snake
         for(int i = 0; i < bodyUnits; i++) {
@@ -78,32 +96,79 @@ public class GamePanel extends JPanel implements ActionListener {
         foodY = random.nextInt(PANEL_HEIGHT / UNIT) * UNIT;
     }
 
+    public void newToxicFood() {
+        ToxicfoodX = random.nextInt(PANEL_WIDTH / UNIT) * UNIT;
+        ToxicfoodY = random.nextInt(PANEL_HEIGHT / UNIT) * UNIT;
+    }
+
     public void movement() {
         for(int i = bodyUnits; i > 0; i--) {
             x[i] = x[i - 1];
             y[i] = y[i - 1];
         }
 
-        if (direction.equals("Right")) {
-            x[0] = x[0] + UNIT;
-        } else if (direction.equals("Left")) {
-            x[0] = x[0] - UNIT;
-        } else if (direction.equals("Up")) {
-            y[0] = y[0] - UNIT;
-        } else if (direction.equals("Down")) {
-            y[0] = y[0] + UNIT;
+        switch (direction) {
+            case "Right" -> x[0] = x[0] + UNIT;
+            case "Left" -> x[0] = x[0] - UNIT;
+            case "Up" -> y[0] = y[0] - UNIT;
+            case "Down" -> y[0] = y[0] + UNIT;
         }
 
     }
 
+    public void checkFood() {
+        if((x[0] == foodX && y[0] == foodY)) {
+            bodyUnits = bodyUnits + 1;
+            foodCounter = foodCounter + 1;
+            if(foodCounter != num) {
+                newFood();
+            } else {
+                newToxicFood();
+            }
+        } else if (x[0] == ToxicfoodX && y[0] == ToxicfoodY) {
+            bodyUnits = bodyUnits / 2;
+            foodCounter = 0;
+            num = random.nextInt(10);
+            newFood();
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(running == true) {
+        if(running) {
             movement();
+            checkFood();
 
         }
         repaint();
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+            switch (keyEvent.getKeyCode()) {
+                case (KeyEvent.VK_RIGHT) -> {
+                    if (!direction.equals("Left")) {
+                        direction = "Right";
+                    }
+                }
+                case (KeyEvent.VK_LEFT) -> {
+                    if (!direction.equals("Right")) {
+                        direction = "Left";
+                    }
+                }
+                case (KeyEvent.VK_UP) -> {
+                    if (!direction.equals("Down")) {
+                        direction = "Up";
+                    }
+                }
+                case (KeyEvent.VK_DOWN) -> {
+                    if (!direction.equals("Up")) {
+                        direction = "Down";
+                    }
+                }
+            }
+        }
     }
 }
