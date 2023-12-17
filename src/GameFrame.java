03a2500ea9ebcca06fb72baf68db3001e3902f54
Hasan Zahid <guszahidha@student.gu.se>
2023-12-17 16:35:05 +0100

    removed the old top panel

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -14,30 +14,11 @@ public class GameFrame {
     public void initialize() {
         this.frame = new JFrame();
         this.frame.setTitle("Slithery Snake");
+        this.frame.add(new GamePanel());
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.frame.setResizable(false);
-
-        JPanel topPanel = new JPanel();
-        topPanel.setPreferredSize(new Dimension(0,50));
-        topPanel.setBackground(new Color(49, 85, 69));
-
-        JButton playButton = new JButton("Play");
-        playButton.setBounds(50,10, 50,30);
-        playButton.setFocusable(false);
-        topPanel.add(playButton);
-
-        ImageIcon logo = new ImageIcon("D:/GitLab Home/group-11/src/Untitled design (1).png");
-        Image logoImage = logo.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        ImageIcon resizedLogo = new ImageIcon(logoImage);
-        JLabel logoLabel = new JLabel(resizedLogo);
-
-        topPanel.setLayout(new BorderLayout());
-        topPanel.add(logoLabel, BorderLayout.WEST);
-
-        this.frame.add(topPanel, BorderLayout.NORTH);
-        this.frame.add(new GamePanel(), BorderLayout.CENTER);
-        this.frame.pack();
         this.frame.setVisible(true);
+        this.frame.pack();
         this.frame.setLocationRelativeTo(null);
         this.stopwatchPanel = new JPanel();
         this.stopwatchLabel = new JLabel();
